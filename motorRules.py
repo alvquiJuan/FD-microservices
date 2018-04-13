ed9cc7e3aaed7ab31afbe0d347df536375758fd8
Juan Manuel Álvarez Quiñonez <juan.alvarez.quinonez@correounivalle.edu.co>
2018-04-12 23:33:14 -0500

    Update backlog Add fuzzy rules add healthy and broken rotor faults

--- motorRules.py
+++ motorRules.py
@@ -9,25 +9,90 @@ Class to evaluate motor damage relevance acording to ATSM E1934-99a
 class ThermalRules:
     def __init__(self):
         #universes for input and output data
-        tc=ctrl.Antecedent(np.arange(0, 45), 'Thermal coefficient')
-        condition=ctrl.Consequent(np.arange(101),'component condition')
+        #we have 4 antecedents (4 thermal signatures)
+        tc1=ctrl.Antecedent(np.arange(0, 45), 'tc1')
+        tc2=ctrl.Antecedent(np.arange(0, 45), 'tc2')
+        tc3=ctrl.Antecedent(np.arange(0, 45), 'tc3')
+        tc4=ctrl.Antecedent(np.arange(0, 45), 'tc4')
+        #one consequent (the motor condition)
+        condition=ctrl.Consequent(np.arange(101),'condition')
+
         #membership functions
         #antecedent functions
-        tc["negligible"]=fuzz.trapmf(tc.universe,[0,0,1,2])
-        tc["low"]=fuzz.trapmf(tc.universe,[1,2,4,5])
-        tc["medium"]=fuzz.trapmf(tc.universe,[4,5,14,15])
-        tc["high"]=fuzz.trapmf(tc.universe,[14,15,29,30])
-        tc["extreme"]=fuzz.trapmf(tc.universe,[29,30,45,45])
-        #uncomment next line to expose antecedent sets 
-        #self.tc=tc
 
-        #consequent rules
-        condition["excelent"]=fuzz.trapmf(condition.universe,[90,95,100,100])
-        condition["ordinary"]=fuzz.trapmf(condition.universe,[75,80,90,95])
-        condition["slight"]=fuzz.trapmf(condition.universe,[60,65,75,80])
-        condition["major"]=fuzz.trapmf(condition.universe,[35,40,60,65])
-        condition["critical"]=fuzz.trapmf(condition.universe,[0,0,35,40])
-        #uncomment next line to expose consequent sets 
+        tc1['negligible']=fuzz.trapmf(tc1.universe,[0,0,1,2])
+        tc1['low']=fuzz.trapmf(tc1.universe,[1,2,4,5])
+        tc1['medium']=fuzz.trapmf(tc1.universe,[4,5,14,15])
+        tc1['high']=fuzz.trapmf(tc1.universe,[14,15,29,30])
+        tc1['extreme']=fuzz.trapmf(tc1.universe,[29,30,45,45])
+
+        tc2['negligible']=fuzz.trapmf(tc2.universe,[0,0,1,2])
+        tc2['low']=fuzz.trapmf(tc2.universe,[1,2,4,5])
+        tc2['medium']=fuzz.trapmf(tc2.universe,[4,5,14,15])
+        tc2['high']=fuzz.trapmf(tc2.universe,[14,15,29,30])
+        tc2['extreme']=fuzz.trapmf(tc2.universe,[29,30,45,45])
+
+        tc3['negligible']=fuzz.trapmf(tc3.universe,[0,0,1,2])
+        tc3['low']=fuzz.trapmf(tc3.universe,[1,2,4,5])
+        tc3['medium']=fuzz.trapmf(tc3.universe,[4,5,14,15])
+        tc3['high']=fuzz.trapmf(tc3.universe,[14,15,29,30])
+        tc3['extreme']=fuzz.trapmf(tc3.universe,[29,30,45,45])
+
+        tc4['negligible']=fuzz.trapmf(tc4.universe,[0,0,1,2])
+        tc4['low']=fuzz.trapmf(tc4.universe,[1,2,4,5])
+        tc4['medium']=fuzz.trapmf(tc4.universe,[4,5,14,15])
+        tc4['high']=fuzz.trapmf(tc4.universe,[14,15,29,30])
+        tc4['extreme']=fuzz.trapmf(tc4.universe,[29,30,45,45])
+
+
+        #uncomment next line to expose antecedent sets
+        #tc3.view()
+
+
+        #consequent functions
+        condition['excelent']=fuzz.trapmf(condition.universe,[90,95,100,100])
+        condition['ordinary']=fuzz.trapmf(condition.universe,[75,80,90,95])
+        condition['slight']=fuzz.trapmf(condition.universe,[60,65,75,80])
+        condition['major']=fuzz.trapmf(condition.universe,[35,40,60,65])
+        condition['critical']=fuzz.trapmf(condition.universe,[0,0,35,40])
+        #uncomment next line to expose consequent sets
         #self.condition=condition
-        
-        
+
+        #fuzzy Rules.
+        rules=[None]*8
+        rules[0]=ctrl.Rule(tc1['negligible']&tc2['negligible']&tc3['negligible']&tc4['negligible'], condition['excelent'],label='HLT')
+        rules[1]=ctrl.Rule(tc1['medium']&tc2['medium']&tc3['medium']&tc4['medium'], condition['ordinary'],label='1/2brb')
+        rules[2]=ctrl.Rule(tc1['low']&tc2['low']&tc3['medium']&tc4['low'], condition['slight'],label='1brb')
+        rules[3]=ctrl.Rule(tc1['medium']&tc2['low']&tc3['low']&tc4['negligible'], condition['major'],label='2brb')
+        rules[4]=ctrl.Rule(tc1['low']&tc2['medium']&tc3['negligible']&tc4['medium'], condition['slight'],label='bd')
+        rules[5]=ctrl.Rule(tc1['medium']&tc2['medium']&tc3['medium']&(tc4['medium']|tc4['high']), condition['major'],label='unb')
+        rules[6]=ctrl.Rule(tc1['negligible']&tc2['negligible']&tc3['negligible']&tc4['low'], condition['slight'],label='vunb')
+        rules[7]=ctrl.Rule(tc1['extreme']&tc2['extreme']&tc3['extreme']&tc4['extreme'], condition['critical'],label='MAL')
+        motor_ctrl = ctrl.ControlSystem(rules)
+
+        #simulation for the motor condition
+        self.motor = ctrl.ControlSystemSimulation(motor_ctrl)
+
+    '''
+    method for motor evaluation
+    the parameters are the thermal coefficient for each component
+    '''
+    def compute_status(self,pf_tc1,pf_tc2,pf_tc3,pf_tc4):
+        processed_tc1=np.clip(pf_tc1,0,45)
+        processed_tc2=np.clip(pf_tc2,0,45)
+        processed_tc3=np.clip(pf_tc3,0,45)
+        processed_tc4=np.clip(pf_tc4,0,45)
+
+        self.motor.input['tc1'] = processed_tc1
+        self.motor.input['tc2'] = processed_tc2
+        self.motor.input['tc3'] = processed_tc3
+        self.motor.input['tc4'] = processed_tc4
+        self.motor.compute()
+        return (self.motor.output['condition'])
+
+myThermal=ThermalRules()
+#healthy motor
+print('healthy motor status:', myThermal.compute_status(0,0,0,0))
+print('half broken rotor motor status:', myThermal.compute_status(6.03,5.63,4.83,5.33))
+print('one broken rotor motor status:', myThermal.compute_status(3.43,3.07,5.03,3.67))
+print('two broken rotor motor status:', myThermal.compute_status(4.5,2.73,2.87,1.4))
