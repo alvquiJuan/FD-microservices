b6afa3f3941fa6b5897bc8193d25b1afdc71751c
Juan Manuel Álvarez Quiñonez <juan.alvarez.quinonez@correounivalle.edu.co>
2018-05-17 11:56:40 -0500

    Create healthy motro experiments

--- motorSimulation.py
+++ motorSimulation.py
@@ -1,11 +1,24 @@
-'''Simulación de un motor'''
+'''motor simulation'''
 from tempObserver import temperatureObserver as to
+import csv
+csvfile="results.csv"
+res=[]
 
 s1=to("Sensor armadura")
 s2=to("Sensor rodamiento")
 s3=to("Sensor polea de motor")
 s4=to("Sensor polea de carga")
-s1.simulateValue(22,50)
+
+'''healthy motor based on García et al Model '''
+for iter in range(1000):
+    s1.simulateValue(40,45)
+    res.append(s1.getValue())
+#print(res)
+
+with open(csvfile, "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    writer.writerow(res)
+'''
 s2.simulateValue(22,50)
 s3.simulateValue(22,50)
 s4.simulateValue(22,50)
@@ -18,3 +31,4 @@ print(s3.getReport())
 print("====")
 print(s4.getReport())
 print("====")
+'''
